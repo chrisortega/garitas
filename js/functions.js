17f99a2d4f90ba37e913878ffda363e2a98c086b
christian ortega <christian-ortega@live.com>
2014-05-15 22:57:16 -0700

    mecos

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 function getData(){
 
   
-var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2506";
+var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2504";
 	
     $.ajax({
         url: baseUrl,
@@ -12,17 +12,32 @@ var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2506";
 
 
           $heads = $(data.responseText).find(".contain_head")
-          getPorts($heads)
+          var ports = getPorts($heads)
+          var x=0;
           $details = $(data.responseText).find(".pass_details")
 
 
 
 
               $.each($details,function(){    
-                console.log(this)
+                console.log(ports[x])
 
               $em =  $(this).find("p").text()  
-                console.log($em)
+                var arr = $em.split("\n") 
+
+                var arc = jQuery.grep(arr, function(n, i){
+                                    n = $.trim(n)
+                                    return (n !== "" && n != null);
+                    }); 
+
+
+                var maxlines = arc[0];
+                for (var i = 1; i <= arc.length-1; i++) {
+                        
+                        console.log(createWaitObject(arc[i]) )
+                };                
+              
+                  x++
           })
 
 
@@ -46,12 +61,23 @@ var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2506";
 
 }
 
+function createWaitObject(string){
+  var obj =  string.split(",");
+  var garita = {
+    "times":'"'+obj[0]+'"',
+    "waits" :'"'+obj[1]+'"',
+    "lines":'"'+obj[2]+'"'
+};
+
+return garita;
+}
+
 function getPorts(heads){
           var ports = [];
           $.each($heads,function(){              
               ports.push($(this).find("strong").text())
           })
-          console.log(ports)
+          //console.log(ports)
           return ports
 }
 
