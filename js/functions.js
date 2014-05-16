88a5ef8123328b0750c8a94552831ffd741c0645
unknown <christiani.ortega@stkend13726.softtek.com>
2014-05-16 13:48:45 -0700

    chupamela chava

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,15 @@
-function getData(){
+$(function() {
+  getData(2504)
+  $(".ports").click(function(){
+    getData(this.id)
+  })
+});
 
+
+function getData(portnumber){
   
-var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2504";
-	
+var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn="+portnumber;
+	var ports
     $.ajax({
         url: baseUrl,
         type: "get",
@@ -12,15 +19,19 @@ var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2504";
 
 
           $heads = $(data.responseText).find(".contain_head")
-          var ports = getPorts($heads)
+          ports = getPorts($heads)
+          
+          var waitsa = []
           var x=0;
+          
+           var out=[];
           $details = $(data.responseText).find(".pass_details")
 
 
 
 
               $.each($details,function(){    
-                console.log(ports[x])
+                
 
               $em =  $(this).find("p").text()  
                 var arr = $em.split("\n") 
@@ -32,31 +43,45 @@ var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2504";
 
 
                 var maxlines = arc[0];
-                for (var i = 1; i <= arc.length-1; i++) {
+                var waits = [];     
+                  
+                
+                for (var i = 1; i <= arc.length-1; i++) {  
                         
-                        console.log(createWaitObject(arc[i]) )
-                };                
-              
-                  x++
+
+                        if (arc[i] == "" || arc[i]==undefined || arc[i] == null || arc[i].indexOf("N/A") >=0 ){}else{
+                          waits.push(createWaitObject(arc[i]))  
+                      }
+
+                };      
+
+                
+                  waitsa.push(waits)                  
+                
+                                
           })
 
+               
+              for (var i = 0; i <= ports.length; i++) {  
+
+                if (waitsa[i]!=undefined){
+                  var garita = []
+
+                  garita["port"]  = ports[i]                       
+                  garita["data"] = waitsa[i] 
+                  
+                  if (waitsa[i].length > 0){
+                   out.push(garita) 
+                  }
+                  
+                  
+                }
+              };
+            
+              printToPanel("main",out)
 
 
 
-
-  			
-        	
-        	/*
-
-            $.each($foop.find('table'), function(idx, item) {
-            	var table = $(item).tableToJSON();
-                console.log(table)
-                /*$("#div_si").html(item)
-                
-            })
-
-        	/*$foop = $('<form>' + data.responseText + '</form>');*/
-        		/*$("#div_si").html(data.responseText)*/
         }});
 
 }
@@ -64,12 +89,42 @@ var baseUrl = "http://apps.cbp.gov/bwt/mobile.asp?action=n&pn=2504";
 function createWaitObject(string){
   var obj =  string.split(",");
   var garita = {
-    "times":'"'+obj[0]+'"',
-    "waits" :'"'+obj[1]+'"',
-    "lines":'"'+obj[2]+'"'
+    "name":obj[0],
+    "wait" :obj[1],
+    "lines":obj[2]
 };
 
 return garita;
+}
+
+function printToPanel(div,info){
+  var titles = "";
+  var tmp = "";
+ 
+  for (var i = 0; i <= info.length; i++) {
+    if (info[i]!=undefined){
+      tmp = info[i].port
+      titles = titles + '<k class="garita">'+ tmp + '</k>'; 
+      titles = titles + "<br>"+processData(info[i].data)
+    }
+  };
+  
+
+  $("#content").html(titles);
+  
+}
+
+function processData(data){
+  var out="";
+  var cl="OK"
+  for (var i = data.length - 1; i >= 0; i--) {
+    cl = thresholds(data[i].wait)
+    out = out + '<hr><k class="subport">'+data[i].name+'</k>';
+    out = out + '<k class="time '+cl+'">'+data[i].wait+'</k>';
+    out = out + "<em>"+data[i].lines+"</em>";
+    out = out + "<br><br>"
+  };
+  return out
 }
 
 function getPorts(heads){
@@ -81,5 +136,30 @@ function getPorts(heads){
           return ports
 }
 
+function thresholds(time){
+  if (time=="no delay") {return "OK";}  
+  var tm = time.split(" ");
+  tm = tm.filter(function(n){ return (n !== "" && n != null) });
+  var wait = tm[0];
+  var scale = tm[1];
+  switch (scale) {
+    case "min":
+      if (wait > 30 ) {
+        return "WARNING";
+      }
+      return "OK";
+    break;
+    case "hr":
+    case "hrs":
+      if (wait <= 1 ) {
+        return "WARNING";
+      }
+      if (wait > 1 ) {
+        return "CRITICAL";
+      }
+    break;
+  }
+  return "OK"
+}
 
 
